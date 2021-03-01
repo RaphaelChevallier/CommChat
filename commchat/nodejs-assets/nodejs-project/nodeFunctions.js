62b1e9e27ceafa109af533a81b47540d07e87cc3
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-02-28 17:06:50 -0800

    added readable node id

--- commchat/nodejs-assets/nodejs-project/nodeFunctions.js
+++ commchat/nodejs-assets/nodejs-project/nodeFunctions.js
@@ -3,13 +3,13 @@ const privateLibp2pNode = require('./nodeP2P')
 
 ;(async () => {
     const node = await privateLibp2pNode()
-
   
     await Promise.all([
-      node.start()
+      node.start(),
     ])
   
-    console.log('nodes started...')
+    console.log('nodes started...' + node.peerId)
+    console.log('nodes started...' + node.multiaddrs)
     // rn_bridge.channel.send('Node was initialized.');
   
   })()
