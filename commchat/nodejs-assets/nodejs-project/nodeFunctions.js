530f31a34c5f9a816806fc9345f93519b056eaa1
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-03-11 11:54:57 -0800

    working datastore that stores after 1 dirtypeer

--- commchat/nodejs-assets/nodejs-project/nodeFunctions.js
+++ commchat/nodejs-assets/nodejs-project/nodeFunctions.js
@@ -1,17 +1,17 @@
 // var rn_bridge = require('rn-bridge');
 const privateLibp2pNode = require('./nodeP2P')
 const PeerId = require('peer-id');
+const multiaddr = require('multiaddr')
 const fs = require('fs');
-const LevelStore = require('datastore-level')
-const store = new LevelStore('./mydb')
 require('dotenv').config()
 const SWARM_KEY = new Uint8Array(process.env.SWARM_KEY.split(","))
-store.close()
 
 function addPeerDB(node, addedNodePeerID, addedNodeMultiAddrs){
-  store.open() //this is to start a new db maybe place in own function
-  node.peerStore.addressBook.add(addedNodePeerID, addedNodeMultiAddrs)
-  store.close()
+  //this is to start a new db maybe place in own function
+  const added = node.peerStore.addressBook.set(addedNodePeerID, addedNodeMultiAddrs)
+  // node.datastore.put(addedNodePeerID, addedNodeMultiAddrs)
+  // node.datastore.has(addedNodePeerID)
+  console.log(added)
 }
 
 async function getIDJSON(){
@@ -34,18 +34,24 @@ async function getIDJSON(){
 ;(async () => {
     const id = await getIDJSON()
     const node = await privateLibp2pNode(SWARM_KEY, id)
-  
+    node.datastore.open()
+
     await Promise.all([
       node.start()
     ])
   
     console.log(`nodes started... ${node.peerId.toB58String()}`)
-    console.log('nodes started...' + node.multiaddrs)
+    console.log('node multiaddr' + node.multiaddrs)
     console.log('node peerID: ' + node.peerId)
     console.log("dialing")
-    // const dialed = await node.dial('/ip4/192.168.1.70/tcp/57336/p2p/QmPHJVgwkH4ApF2pPQ4UDCUEzhfM4oJ9hqncmDaawU9coq')
-    // console.log("Dialed: " + dialed)
-    // addPeerDB(node,'QmPHJVgwkH4ApF2pPQ4UDCUEzhfM4oJ9hqncmDaawU9coq', '/ip4/127.0.0.1/tcp/57336,/ip4/100.244.186.88/tcp/57336,/ip4/192.168.1.70/tcp/57336')
+    const dialed = await node.dial(PeerId.createFromB58String('QmPHJVgwkH4ApF2pPQ4UDCUEzhfM4oJ9hqncmDaawU9coq'))
+    console.log("Dialed: " + dialed)
+    var arrayOf = [multiaddr('/ip4/127.0.0.1/tcp/57336'),multiaddr('/ip4/100.244.186.88/tcp/57336'),multiaddr('/ip4/192.168.1.70/tcp/57336')]
+    var peerIDPhone = PeerId.createFromB58String('bafzbeiaoaalj5bktm6n5z7ilnpaqdefa3ajtr5dmelw3lsk3z2zqmtey7a')
+    // addPeerDB(node, peerIDPhone, arrayOf)
+    node.datastore.close()
+    node.stop()
+
     // rn_bridge.channel.send('Node was initialized.');
   
   })()
