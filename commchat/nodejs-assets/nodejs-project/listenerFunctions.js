2925f43ee995ec400a069513c7682879f3b68d7b
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-22 01:11:37 -0700

    hangup again

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -7,15 +7,8 @@ async function listen(node) {
     console.log('connected to: ', connection.remotePeer.toB58String())
   })
 
-  node.connectionManager.on('peer:disconnect', (connection) => {
-    node.unhandle('/chat/1.0.0')
-     // Handle messages for the protocol
-    await node.handle('/chat/1.0.0', async ({ stream }) => {
-      // Send stdin to the stream
-      stdinToStream(stream)
-      // Read the stream and output to console
-      streamToConsole(stream)
-    })
+  await node.connectionManager.on('peer:disconnect', (connection) => {
+    await node.hangup(connection.remotePeer)
     console.log('disconnected peer')
   })
 
