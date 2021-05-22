9c860a1b5074cbf670c9c7f19977e7e931bbf1d2
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-22 12:04:32 -0700

    try to handle chat on connection basis

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -4,19 +4,13 @@ const PeerId = require('peer-id')
 async function listen(node) {
   // Log a message when a remote peer connects to us
   node.connectionManager.on('peer:connect', (connection) => {
+    handleChat()
     console.log('connected to: ', connection.remotePeer.toB58String())
-    console.log(connection.streams)
   })
 
   node.connectionManager.on('peer:disconnect', (connection) => {
-<<<<<<< HEAD
-    console.log('disconnected peer: ', connection.remotePeer.toB58String())
-    connection.tags
-=======
-    node.hangUp(connection.remotePeer)
-    handleChat()
+    connection.close();
     console.log('disconnected peer')
->>>>>>> main
   })
 
   async function handleChat () {
@@ -28,22 +22,6 @@ async function listen(node) {
       streamToConsole(stream)
     })
   }
-
-<<<<<<< HEAD
-=======
-  async function handleChat () {
-    // Handle messages for the protocol
-    await node.handle('/chat/1.0.0', async ({ stream }) => {
-      // Send stdin to the stream
-      stdinToStream(stream)
-      // Read the stream and output to console
-      streamToConsole(stream)
-    })
-  }
-
->>>>>>> main
-  handleChat()
-
   // Output listen addresses to the console
   console.log('Listener ready, listening on:')
   node.multiaddrs.forEach((ma) => {
