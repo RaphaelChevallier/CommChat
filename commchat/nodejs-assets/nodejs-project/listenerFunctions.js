0f1df52d51b6df90500f8c809a1df895901898a6
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-22 14:15:53 -0700

    stash bidirectional

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -4,7 +4,7 @@ const PeerId = require('peer-id')
 
 async function handleChat () {
   // Handle messages for the protocol
-  await node.handle('/chat/laptop', async ({ stream }) => {
+  await node.handle('/chat/phone', async ({ stream }) => {
     // Send stdin to the stream
     stdinToStream(stream)
     // Read the stream and output to console
@@ -16,7 +16,7 @@ async function listen(node) {
   // Log a message when a remote peer connects to us
   node.connectionManager.on('peer:connect', (connection) => {
     handleChat()
-    dial(node, '/chat/phone') //make it bidirectional
+    // dial(node, '/chat/phone') //make it bidirectional
     console.log('connected to: ', connection.remotePeer.toB58String())
   })
 
