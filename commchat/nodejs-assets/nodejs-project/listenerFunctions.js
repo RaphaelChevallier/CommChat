82f7710afd79335bc48c5cf3654ab63e3468614f
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-22 11:39:40 -0700

    check streams

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -5,11 +5,12 @@ async function listen(node) {
   // Log a message when a remote peer connects to us
   node.connectionManager.on('peer:connect', (connection) => {
     console.log('connected to: ', connection.remotePeer.toB58String())
+    console.log(connection.streams)
   })
 
   node.connectionManager.on('peer:disconnect', (connection) => {
-    connection.close()
     console.log('disconnected peer: ', connection.remotePeer.toB58String())
+    console.log(connection.streams)
   })
 
 
