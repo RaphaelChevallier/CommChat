9af012dd2e452d81d011cb65e91e849419acd6e2
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-23 15:54:33 -0700

    unhandle connections

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -21,6 +21,7 @@ async function listen(node) {
   })
 
   node.connectionManager.on('peer:disconnect', (connection) => {
+    node.unhandle('/chat/laptop')
     console.log('disconnected peer')
   })
 
