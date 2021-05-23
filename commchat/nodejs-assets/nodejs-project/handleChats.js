49af49a8768b2efb6a7e8a4f62afa2cb87119ce7
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-23 16:06:38 -0700

    handle

---
+++ commchat/nodejs-assets/nodejs-project/handleChats.js
@@ -0,0 +1,15 @@
+const { stdinToStream, streamToConsole } = require('./stream')
+
+async function handleChat(node) {
+    // Handle messages for the protocol
+    await node.handle('/chat/phone', async ({ stream }) => {
+      // Send stdin to the stream
+      stdinToStream(stream)
+      // Read the stream and output to console
+      streamToConsole(stream)
+    })
+  }
+
+  module.exports = {
+    handleChat
+  }
