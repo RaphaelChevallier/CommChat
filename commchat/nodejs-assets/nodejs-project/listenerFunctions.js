416b6b30666e94691660947d31f8373056d6a897
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-23 16:22:10 -0700

    handling

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -1,20 +1,11 @@
 const { stdinToStream, streamToConsole } = require('./stream')
 const { dial } = require('./dialerFunctions')
-const PeerId = require('peer-id')
-
-async function handleChat(node) {
-  // Handle messages for the protocol
-  await node.handle('/chat/laptop', async ({ stream }) => {
-    // Send stdin to the stream
-    stdinToStream(stream)
-    // Read the stream and output to console
-    streamToConsole(stream)
-  })
-}
+const { handleChat } = require('./handleChats')
 
 async function listen(node) {
   // Log a message when a remote peer connects to us
   node.connectionManager.on('peer:connect', (connection) => {
+    handleChat(node)
     // dial(node, '/chat/phone') //make it bidirectional
     console.log('connected to: ', connection.remotePeer.toB58String())
   })
