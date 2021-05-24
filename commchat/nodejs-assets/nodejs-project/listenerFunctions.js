d054b1a2b1c3d3fef0ff9133868e5f76ed91beae
Raphael Chevallier <raphaelchevallier@hotmail.com>
2021-05-23 17:07:03 -0700

    try

--- commchat/nodejs-assets/nodejs-project/listenerFunctions.js
+++ commchat/nodejs-assets/nodejs-project/listenerFunctions.js
@@ -26,6 +26,16 @@ async function listen(node) {
 
   handleChat(node)
 
+  async function handleChat(node) {
+    // Handle messages for the protocol
+    await node.handle('/chat/phone', async ({ stream }) => {
+      // Send stdin to the stream
+      stdinToStream(stream)
+      // Read the stream and output to console
+      streamToConsole(stream)
+    })
+  }
+
 
   // Output listen addresses to the console
   console.log('Listener ready, listening on:')
